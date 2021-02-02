d72c0f9514d29def0cddc2928f90898671d1c1c5
cmanish049 <cmanish049@gmail.com>
2021-02-02 17:41:31 +0545

    changes after data retrieved from db

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
-connectDB();
+
+connectDB()
 
 const app = express();
+
 app.get('/', (req, res) => {
     res.send('This is backend')
 })
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
 
+app.use('/api/products', productRoutes)
+app.use(notFound)
+app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 app.listen(5000, console.log(`Started Server at ${PORT}`));
